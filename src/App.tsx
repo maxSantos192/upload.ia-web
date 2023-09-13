0562235f57d180f9277098c87f74eff453e1f1f7
maxsantos <maxsonsantos88@gmail.com>
2023-09-13 12:30:48 -0300

    Layout update

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ export function App() {
 
         <div className="flex items-center gap-3">
           <span className="text-sm text-muted-foreground">
-            Desenvolvido com 💜
+            Desenvolvido com 💜 by Max Santos
           </span>
 
           <Separator orientation="vertical" className="h-6" />
